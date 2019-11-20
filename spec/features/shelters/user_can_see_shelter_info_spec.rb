da471b04e95db8ad055d40226a14c40196430679
Alison Vermeil <alison.minton@gmail.com>
2019-11-19 17:24:24 -0700

    Add test for see_shelter_info

---
+++ spec/features/shelters/user_can_see_shelter_info_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "shelter show page", type: :feature do
+  it "can see shelter info" do
+    shelter_1 = Shelter.create(name: "Boulder Shelter",
+                              address: "123 Arapahoe Ave",
+                              city: "Boulder",
+                              state: "CO",
+                              zip: "80301")
+    # shelter_2 = Shelter.create(name: "Denver Shelter",
+    #                           address: "345 Blake St",
+    #                           city: "Denver",
+    #                           state: "CO",
+    #                           zip: "80220")
+
+    visit "/shelters"
+
+    expect(page).to have_link 'Add Shelter', href: '/shelters/new'
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+  end
+end
