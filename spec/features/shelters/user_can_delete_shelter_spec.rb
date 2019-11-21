05e08b42de000a05d5692ac86ced56133ab3227e
Alison Vermeil <alison.minton@gmail.com>
2019-11-20 21:17:30 -0700

    Complete test for delete_shelter

--- spec/features/shelters/user_can_delete_shelter_spec.rb
+++ spec/features/shelters/user_can_delete_shelter_spec.rb
@@ -7,12 +7,15 @@ RSpec.describe "delete shelter", type: :feature do
                               city: "Boulder",
                               state: "CO",
                               zip: "80301")
+    visit "/shelters"
+    expect(page).to have_content(shelter_1.name)
 
     visit "/shelters/#{shelter_1.id}"
-
     expect(page).to have_link('Delete Shelter')
 
     click_link 'Delete Shelter'
 
+    expect(current_path).to eq("/shelters")
+    expect(page).to_not have_content(shelter_1.name)
   end
-end 
+end
