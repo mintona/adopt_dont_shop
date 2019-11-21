23ce172eac44bbcdc573c914919b9ed47b0c208a
Alison Vermeil <alison.minton@gmail.com>
2019-11-20 17:12:34 -0700

    Reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get '/', to: 'shelters#index'
+  get '/', to: 'welcome#index'
+  # get '/', to: 'shelters#index'
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
+
   get '/shelters/:id', to: 'shelters#show'
+  post '/shelters', to: 'shelters#create'
 end
