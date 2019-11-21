66ea14dcd9aa88dcca3d41e309d71dd7a0ac7b35
Alison Vermeil <alison.minton@gmail.com>
2019-11-20 19:48:57 -0700

    Add tests for editing shelter

---
+++ spec/features/shelters/user_can_update_shelter_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "edit shelter", type: :feature do
+  it "can update existing shelter" do
+    shelter_1 = Shelter.create(name: "Boulder Shelter",
+                              address: "123 Arapahoe Ave",
+                              city: "Boulder",
+                              state: "CO",
+                              zip: "80301")
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_link('Update Shelter')
+
+    click_on 'Update Shelter'
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+
+    expect(page).to have_content('Name')
+    expect(page).to have_content('Address')
+    expect(page).to have_content('City')
+    expect(page).to have_content('State')
+    expect(page).to have_content('Zip')
+
+    # fill out all the stuff you want to change 
+    fill_in 'Name', with: 'Boulder Animal Shelter'
+
+    expect(page).to have_button('Update Shelter')
+
+    click_on 'Update Shelter'
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+
+    expect(page).to have_content('Boulder Animal Shelter')
+    expect(pate).to_not have_content('Boulder Shelter')
+
+  end
+end
