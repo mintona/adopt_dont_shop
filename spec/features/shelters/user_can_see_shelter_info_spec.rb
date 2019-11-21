8c7418b3af05ef594c9f66804d5beeca5c3d2cd4
Alison Vermeil <alison.minton@gmail.com>
2019-11-20 19:46:21 -0700

    Add another shelter to tests

--- spec/features/shelters/user_can_see_shelter_info_spec.rb
+++ spec/features/shelters/user_can_see_shelter_info_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe "shelter show page", type: :feature do
                               city: "Boulder",
                               state: "CO",
                               zip: "80301")
-    # shelter_2 = Shelter.create(name: "Denver Shelter",
-    #                           address: "345 Blake St",
-    #                           city: "Denver",
-    #                           state: "CO",
-    #                           zip: "80220")
+    shelter_2 = Shelter.create(name: "Denver Shelter",
+                              address: "345 Blake St",
+                              city: "Denver",
+                              state: "CO",
+                              zip: "80220")
 
     visit "/shelters/#{shelter_1.id}"
 
@@ -20,5 +20,16 @@ RSpec.describe "shelter show page", type: :feature do
     expect(page).to have_content(shelter_1.city)
     expect(page).to have_content(shelter_1.state)
     expect(page).to have_content(shelter_1.zip)
+    expect(page).to_not have_content(shelter_2.name)
+
+    visit "/shelters/#{shelter_2.id}"
+
+    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content(shelter_2.address)
+    expect(page).to have_content(shelter_2.city)
+    expect(page).to have_content(shelter_2.state)
+    expect(page).to have_content(shelter_2.zip)
+    expect(page).to_not have_content(shelter_1.name)
+
   end
 end
