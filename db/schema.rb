f8705abce18992676fba66ff882a44e111a61278
Alison Vermeil <alison.minton@gmail.com>
2019-11-20 19:45:55 -0700

    Add timestamps to table and reset tables

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20191119223432) do
     t.string "city"
     t.string "state"
     t.string "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
