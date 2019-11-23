28e780bc750894a5a25b3a54f08321449b364399
Alison Vermeil <alison.minton@gmail.com>
2019-11-23 13:06:32 -0700

    Refactor pets show tests

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -10,8 +10,13 @@ RSpec.describe "As a visitor" do
                                 zip: "80301")
 
       pet_1_image = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
-
-      pet_1 = Pet.create!(image: pet_1_image, name: "Alex", approximate_age: "10", sex: "Male", shelter: shelter_1)
+      pet_1_description = "I am a loveable, snuggly, cat! If you are anti snuggle, look elsewhere. I want to be pet at all times!"
+      pet_1 = Pet.create!(image: pet_1_image,
+                          name: "Alex",
+                          approximate_age: "10",
+                          sex: "Male",
+                          shelter: shelter_1,
+                          description: pet_1_description)
 
       shelter_2 = Shelter.create(name: "Denver Shelter",
                                 address: "345 Blake St",
@@ -20,15 +25,21 @@ RSpec.describe "As a visitor" do
                                 zip: "80220")
 
       pet_2_image = "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg"
-      pet_2 = Pet.create!(image: pet_2_image, name: "Marley", approximate_age: "2", sex: "Female", shelter: shelter_2)
+      pet_2_description = 'I am the cutest puppy ever! I love to be around kids as long as they do not play too "ruff."'
+      pet_2 = Pet.create!(image: pet_2_image,
+                          name: "Marley",
+                          approximate_age: "2",
+                          sex: "Female",
+                          shelter: shelter_2,
+                          description: pet_2_description)
 
       visit "/pets/#{pet_1.id}"
-
+require "pry"; binding.pry
       expect(page).to have_content("#{pet_1.name}")
       expect(page).to have_content("#{pet_1.description}")
       expect(page).to have_content("#{pet_1.approximate_age}")
       expect(page).to have_content("#{pet_1.sex}")
-      expect(page).to have_content("Adoption Status:")
+      expect(page).to have_content("Adoption Status: Adoptable")
       expect(page).to have_css("img[src*='#{pet_1_image}']")
     end
   end
