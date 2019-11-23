028666ae8f48c17b33383590ba0856e0509b8999
Alison Vermeil <alison.minton@gmail.com>
2019-11-23 11:42:15 -0700

    Add route and idex action to pets index to show only pets at that shelter

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,12 @@
 class PetsController < ApplicationController
 
   def index
-    @pets = Pet.all
+    if params[:shelter_id]
+      @shelter = Shelter.find(params[:shelter_id])
+      @pets = @shelter.pets
+    else
+      @pets = Pet.all
+    end
   end
 
   def show
