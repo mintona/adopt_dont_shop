9ac491c1afd91a5b7a91b8aadcb1a9a01e0ebdc2
Alison Vermeil <alison.minton@gmail.com>
2019-11-23 15:01:04 -0700

    Add descriptions to pet and a new shelter to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,28 @@ shelter_2 = Shelter.create(name: "Denver Shelter",
                           state: "CO",
                           zip: "80220")
 
+shelter_3 = Shelter.create(name: "Beverly Hills Shelter",
+                          address: "414 Rodeo Dr",
+                          city: "Beverly Hills",
+                          state: "CA",
+                          zip: "90210")
+
+
 pet_1_image = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
+pet_1_description = "I am a loveable, snuggly, cat! If you are anti snuggle, look elsewhere. I want to be pet at all times!"
+
 pet_1 = Pet.create!(image: pet_1_image,
                     name: "Alex",
                     approximate_age: "10",
                     sex: "male",
-                    shelter: shelter_1)
+                    shelter: shelter_1,
+                    description: pet_1_description)
 
 pet_2_image = "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg"
-pet_2 = Pet.create!(image: pet_2_image, name: "Marley", approximate_age: "2", sex: "female", shelter: shelter_2)
+pet_2_description = 'I am the cutest puppy ever! I love to be around kids as long as they do not play too "ruff."'
+pet_2 = Pet.create!(image: pet_2_image,
+                    name: "Marley",
+                    approximate_age: "2",
+                    sex: "female",
+                    shelter: shelter_2,
+                    description: pet_2_description)
