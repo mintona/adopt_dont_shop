51d344bd06bc7bbc9c8d1365126d79262e0c7d7e
Alison Vermeil <alison.minton@gmail.com>
2019-11-23 19:11:00 -0700

    Add route for deleting pet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
-  
+
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get '/pets/:id', to: 'pets#show'
   get '/pets/:id/edit', to: 'pets#edit'
   patch '/pets/:id', to: 'pets#update'
+  delete '/pets/:id', to: 'pets#destroy'
 
   get '/shelters/:shelter_id/pets', to: 'pets#index'
   get '/shelters/:shelter_id/pets/new', to: 'pets#new'
