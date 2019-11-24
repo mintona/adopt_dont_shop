6a3d45c6340ded11dfcd512ac042efac91688e35
Alison Vermeil <alison.minton@gmail.com>
2019-11-24 14:23:32 -0700

    Add test for shelter names linking to shelter show page

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -17,13 +17,17 @@ RSpec.describe "As a visitor", type: :feature do
       visit "/shelters"
     end
 
-    it "I can see all shelters names" do
+    it "I can see all shelters names as links" do
 
       expect(page).to have_content(@shelter_1.name)
       # expect(page).to have_content("#{shelter_1.name}")
 
       expect(page).to have_content(@shelter_2.name)
       # expect(page).to have_content("#{shelter_2.name}")
+      expect(page).to have_link("#{@shelter_2.name}")
+      click_on("#{@shelter_1.name}")
+
+      expect(current_path).to eq("/shelters/#{@shelter_1.id}")
     end
 
     it "I can click a link to edit each shelter" do
