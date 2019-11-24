94cac90858bb9b10bf9ecad45e20f2a4722164c6
Alison Vermeil <alison.minton@gmail.com>
2019-11-24 11:59:08 -0700

    Add setup to test file and refactor accordingly

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor", type: :feature do
-  describe "When I visits the pets index" do
-    it "I can see each pets info" do
-      shelter_1 = Shelter.create(name: "Boulder Shelter",
+  describe "When I visit the pets index" do
+    before(:each) do
+      @shelter_1 = Shelter.create(name: "Boulder Shelter",
                                 address: "123 Arapahoe Ave",
                                 city: "Boulder",
                                 state: "CO",
@@ -11,31 +11,42 @@ RSpec.describe "As a visitor", type: :feature do
 
       pet_1_image = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
 
-      pet_1 = Pet.create!(image: pet_1_image, name: "Alex", approximate_age: "10", sex: "Male", shelter: shelter_1)
+      @pet_1 = Pet.create!(image: pet_1_image,
+                          name: "Alex",
+                          approximate_age: "10",
+                          sex: "Male",
+                          shelter: @shelter_1)
 
-      shelter_2 = Shelter.create(name: "Denver Shelter",
+      @shelter_2 = Shelter.create(name: "Denver Shelter",
                                 address: "345 Blake St",
                                 city: "Denver",
                                 state: "CO",
                                 zip: "80220")
 
       pet_2_image = "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg"
-      pet_2 = Pet.create!(image: pet_2_image, name: "Marley", approximate_age: "2", sex: "Female", shelter: shelter_2)
+      @pet_2 = Pet.create!(image: pet_2_image,
+                          name: "Marley",
+                          approximate_age: "2",
+                          sex: "Female",
+                          shelter: @shelter_2)
 
-      visit '/pets'
+        visit '/pets'
+    end
 
-      expect(page).to have_content('Name: Alex')
-      expect(page).to have_content('Approximate Age: 10')
-      expect(page).to have_content('Sex: Male')
-      expect(page).to have_content('Current Shelter: Boulder Shelter')
-      expect(page).to have_css("img[src*='#{pet_1_image}']")
+    it "I can see each pets info" do
+
+      expect(page).to have_content("Name: #{@pet_1.name}")
+      expect(page).to have_content("Approximate Age: #{@pet_1.approximate_age}")
+      expect(page).to have_content("Sex: #{@pet_1.sex}")
+      expect(page).to have_content("Current Shelter: #{@pet_1.shelter.name}")
+      expect(page).to have_css("img[src*='#{@pet_1.image}']")
       #how to test for the alt text
 
-      expect(page).to have_content('Name: Marley')
-      expect(page).to have_content('Approximate Age: 2')
-      expect(page).to have_content('Sex: Female')
-      expect(page).to have_content('Current Shelter: Denver Shelter')
-      expect(page).to have_css("img[src*='#{pet_2_image}']")
+      expect(page).to have_content("Name: #{@pet_2.name}")
+      expect(page).to have_content("Approximate Age: #{@pet_2.approximate_age}")
+      expect(page).to have_content("Sex: #{@pet_2.sex}")
+      expect(page).to have_content("Current Shelter: #{@pet_2.shelter.name}")
+      expect(page).to have_css("img[src*='#{@pet_2.image}']")
 
     end
   end
