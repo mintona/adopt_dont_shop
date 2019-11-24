e9d89b66fb38caed0b103392ca43e2ce0c3bf048
Alison Vermeil <alison.minton@gmail.com>
2019-11-24 11:04:09 -0700

    Add test for delete link for each shelter on shelter index

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -1,49 +1,62 @@
 require 'rails_helper'
 
-RSpec.describe "shelters index page", type: :feature do
-  before(:each) do
-    @shelter_1 = Shelter.create(name: "Boulder Shelter",
-                              address: "123 Arapahoe Ave",
-                              city: "Boulder",
-                              state: "CO",
-                              zip: "80301")
-    @shelter_2 = Shelter.create(name: "Denver Shelter",
-                              address: "345 Blake St",
-                              city: "Denver",
-                              state: "CO",
-                              zip: "80220")
+RSpec.describe "As a visitor", type: :feature do
+  describe "when I visit the shelters index page" do
+    before(:each) do
+      @shelter_1 = Shelter.create(name: "Boulder Shelter",
+                                address: "123 Arapahoe Ave",
+                                city: "Boulder",
+                                state: "CO",
+                                zip: "80301")
+      @shelter_2 = Shelter.create(name: "Denver Shelter",
+                                address: "345 Blake St",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80220")
 
-    visit "/shelters"
-  end
-
-  it "can see all shelters names" do
-
-    expect(page).to have_content(@shelter_1.name)
-    # expect(page).to have_content("#{shelter_1.name}")
-
-    expect(page).to have_content(@shelter_2.name)
-    # expect(page).to have_content("#{shelter_2.name}")
-  end
-
-  it "can see a link to edit each shelter" do
-    #is this right??? is this enough??
-    within(:css, "section##{@shelter_1.id}") do
-      click_on 'Edit Shelter'
-      expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
+      visit "/shelters"
     end
 
-    visit '/shelters'
+    it "I can see all shelters names" do
 
-    within(:css, "section##{@shelter_2.id}") do
-      click_on 'Edit Shelter'
-      expect(current_path).to eq("/shelters/#{@shelter_2.id}/edit")
+      expect(page).to have_content(@shelter_1.name)
+      # expect(page).to have_content("#{shelter_1.name}")
+
+      expect(page).to have_content(@shelter_2.name)
+      # expect(page).to have_content("#{shelter_2.name}")
     end
 
-# save_and_open_page
-#     expect(page).to have_css("##{@shelter_1.id}", :text => 'Edit Shelter')
-#     # within... click_link('Edit Shelter')
-#
-#     expect(page).to have_css("##{@shelter_2.id}", :text => 'Edit Shelter')
+    it "I can click a link to edit each shelter" do
 
+      within(:css, "section##{@shelter_1.id}") do
+        click_on 'Edit Shelter'
+        expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
+      end
+
+      visit '/shelters'
+
+      within(:css, "section##{@shelter_2.id}") do
+        click_on 'Edit Shelter'
+        expect(current_path).to eq("/shelters/#{@shelter_2.id}/edit")
+      end
+    end
+
+    it "I can click a link to delete each shelter" do
+      within(:css, "section##{@shelter_1.id}") do
+        click_on 'Delete Shelter'
+
+        expect(current_path).to eq("/shelters")
+        expect(current_page).to_not have_content(@shelter_1.name)
+      end
+
+      visit '/shelters'
+
+      within(:css, "section##{@shelter_2.id}") do
+        click_on 'Delete Shelter'
+        expect(current_path).to eq("/shelters")
+        expect(current_page).to_not have_content(@shelter_2.name)
+
+      end
+    end
   end
 end
