6a2ebe272b3b2d676cfb6d83504ffbedbef6678c
Alison Vermeil <alison.minton@gmail.com>
2019-11-23 18:38:19 -0700

    Add test file for deleting pets

---
+++ spec/features/pets/destroy_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "when I visit a pet show page" do
+    before(:each) do
+        @shelter_1 = Shelter.create(name: "Boulder Shelter",
+                                    address: "123 Arapahoe Ave",
+                                    city: "Boulder",
+                                    state: "CO",
+                                    zip: "80301")
+
+        @image = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
+        @description = "I am a loveable, snuggly, cat! If you are anti-snuggle, look elsewhere. I want to be pet at all times!"
+        @name = "Alex"
+        @age = "10"
+        @sex = "Male"
+        @pet_1 = @shelter_1.pets.create!(name: @name,
+                                        image: @image,
+                                        approximate_age: @age,
+                                        sex: @sex,
+                                        description: @description)
+    end
+
+    it "I see a link to delete the pet" do
+      visit "/pets/#{@pet_1.id}"
+
+      has_link 'Delete Pet'
+      # expect(current_path).to eq("/pets/#{@pet_1.id}/edit")
+    end
+
+    it "I click the delete pet link to delete the pet" do
+
+    end
+  end
+end
