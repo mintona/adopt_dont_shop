a2bf013adb33275bd9d158971847e31e28692b66
Alison Vermeil <alison.minton@gmail.com>
2019-11-23 17:18:49 -0700

    Add controller action, routes and views for adding new pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,4 +13,20 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
 
+  def new
+    @shelter_id = params[:shelter_id]
+  end
+
+  def create
+    pet = Pet.new(pet_params)
+    pet.shelter_id = params[:shelter_id]
+    pet.save!
+    redirect_to "/shelters/#{pet.shelter_id}/pets"
+  end
+
+  private
+    def pet_params
+      params.permit(:image, :name, :description, :approximate_age, :sex)
+    end
+
 end
