5b4fa2d84b0d7d0e9ace02053eb42773421c56dc
Alison Vermeil <alison.minton@gmail.com>
2019-11-26 12:03:29 -0500

    Add assertions for auto populated form and remove redundancies

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -1,41 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe "edit shelter", type: :feature do
-  it "can update existing shelter" do
-    shelter_1 = Shelter.create(name: "Boulder Shelter",
-                              address: "123 Arapahoe Ave",
-                              city: "Boulder",
-                              state: "CO",
-                              zip: "80301")
+RSpec.describe "As a visitor", type: :feature do
+  it "I can update the information of an existing shelter" do
+    original_name = "Boulder Shelter"
+    original_address = "123 Arapahoe Ave"
+    original_city = "Boulder"
+    original_state = "CO"
+    original_zip = "80301"
+
+    shelter_1 = Shelter.create(name: original_name,
+                              address: original_address,
+                              city: original_city,
+                              state: original_state,
+                              zip: original_zip)
 
     visit "/shelters/#{shelter_1.id}"
 
-    expect(page).to have_link('Update Shelter')
+    expect(page).to have_content(original_name)
+    expect(page).to have_content(original_address)
+    expect(page).to have_content(original_city)
+    expect(page).to have_content(original_state)
+    expect(page).to have_content(original_zip)
 
     click_link 'Update Shelter'
 
     expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
 
-    expect(page).to have_content('Name')
-    expect(page).to have_content('Address')
-    expect(page).to have_content('City')
-    expect(page).to have_content('State')
-    expect(page).to have_content('Zip')
+    expect(find_field('Name').value).to eq(original_name)
+    expect(find_field('Address').value).to eq(original_address)
+    expect(find_field('City').value).to eq(original_city)
+    expect(find_field('State').value).to eq(original_state)
+    expect(find_field('Zip').value).to eq(original_zip)
 
-    # how do i test that the form auto populated with existing info?
-    #expect(find_field('name').value.to eq -->"Boulder Shelter"
+    new_name = "The Greatest Shelter Ever"
+    new_zip = "80304"
 
-    # fill out all the stuff you want to change
-    fill_in 'Name', with: 'Boulder Animal Shelter'
-
-    expect(page).to have_button('Update Shelter')
+    fill_in 'Name', with: new_name
+    fill_in 'Zip', with: new_zip
 
     click_on 'Update Shelter'
 
     expect(current_path).to eq("/shelters/#{shelter_1.id}")
 
-    expect(page).to have_content('Boulder Animal Shelter')
-    expect(page).to_not have_content('Boulder Shelter')
-# Should all new information be filled out every time they visit this form??
+    expect(page).to_not have_content(original_name)
+    expect(page).to_not have_content(original_zip)
+
+    expect(page).to have_content(new_name)
+    expect(page).to have_content(original_address)
+    expect(page).to have_content(original_city)
+    expect(page).to have_content(original_state)
+    expect(page).to have_content(new_zip)
   end
 end
